1215cc57f89f8123048bc501866cf29ec41f23e0
Juliver Napitupulu <juliver.napitupulu@contact-software.com>
2021-11-06 12:29:00 +0100

    utilize react-router

--- src/components/scatter-plot.jsx
+++ src/components/scatter-plot.jsx
@@ -3,7 +3,6 @@ import {max} from 'd3-array';
 import {scaleLinear} from 'd3-scale';
 import DataCircles from './data-circles';
 import XYAxis from './x-y-axis';
-import Network from './network';
 
 const xMax = (data) => max(data, (d) => d[0]);
 const yMax = (data) => max(data, (d) => d[1]);
@@ -26,12 +25,9 @@ const marshalProps = (props) => {
 export default (props) => {
     const d3Props = marshalProps(props);
     return (
-    <>
         <svg width={d3Props.width} height={d3Props.height}>
             <DataCircles {...d3Props} />
             <XYAxis {...d3Props} />
         </svg>
-        <Network/>
-    </>
     );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,59 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {makeStore} from './lib/store';
 import Chart from './components/chart.jsx';
+import Network from './components/network';
+import {
+  Route,
+  Routes,
+  BrowserRouter,
+  Link,
+  Outlet
+} from 'react-router-dom';
 import './main.css';
 
 const store = makeStore();
-const app = <Provider store={store}>
-  <Chart />
-</Provider>;
+
+const app = (
+  <Provider store={store}>
+    <Chart />
+  </Provider>
+);
+
+const layout = (
+  <div>
+    Home sweet home!
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/graph">Graph</Link>
+        </li>
+        <li>
+          <Link to="/network">Network</Link>
+        </li>
+      </ul>
+    </nav>
+    <Outlet/>
+  </div>
+);
+
+const master = (
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={layout}>
+          <Route path="graph" element={app}/>
+          <Route path="network" element={<Network/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
 const mountingPoint = document.createElement('div');
 mountingPoint.className = 'react-app';
 document.body.appendChild(mountingPoint);
 
-ReactDOM.render(app, mountingPoint);
+ReactDOM.render(master, mountingPoint);
